f34a1d14cc67b97b76989b63adea197e980d2606
Andrew Mathenge <santa@northpole.com>
2020-07-24 19:19:25 +0300

    Added JQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $("textarea").keypress(function(event){
 	if(event.which === 13){
         var name= $("input[type='text']").val();
-        alert(name)
+        alert("Thank you"+" "+name+" "+"for the input")
 	}
+})
+$(".click").on("click", function(){
+    $(this).fadeUp()
 })
