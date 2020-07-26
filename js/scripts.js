b7ca16fb5665cb7f5959894b4edab9b1919b8068
Andrew Mathenge <santa@northpole.com>
2020-07-26 14:28:05 +0300

    Testing JQuery functions

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,15 @@ $("textarea").keypress(function(event){
         alert("Thank you"+" "+name+" "+"for the input")
 	}
 })
+$(".click1").click(function(){
+    $(".hide1").slideToggle(1000);
+    $(".show1").slideToggle(1200);
+  });
+$(".click2").click(function(){
+    $(".hide2").slideToggle(1000);
+    $(".show1").slideToggle(1200);
+  });
+$(".click3").click(function(){
+    $(".hide3").slideToggle(1000);
+    $(".show1").slideToggle(1200);
+  });  
